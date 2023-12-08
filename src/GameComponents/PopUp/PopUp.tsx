1babccba8295b1c64ff563aaf70e6f0f616d6796
0CT0PUCE-LOG <soren.starck@etu.umontpellier.fr>
2023-12-08 05:58:58 +0100

    refractor popup

--- src/GameComponents/PopUp/PopUp.tsx
+++ src/GameComponents/PopUp/PopUp.tsx
@@ -22,7 +22,7 @@ const PopUp: React.FC<PopUpProps> = ({choices, valeurEcologie, valeurEconomie, v
         <div className="popup h-full w-full absolute top-0 left-0 flex justify-center items-center" style={{
             zIndex: 1000
         }}>
-            <Card withBorder className={" w-[400px] overflow-y-auto"}>
+            <Card withBorder className={" w-[700px] overflow-y-auto"}>
                 <div className={"flex flex-col gap-1"}>
                     <h2>Résumé de vos choix :</h2>
                     <ul>
@@ -33,12 +33,14 @@ const PopUp: React.FC<PopUpProps> = ({choices, valeurEcologie, valeurEconomie, v
                         ))}
                     </ul>
                     <h3>Résultats :</h3>
-                    <p>Valeur Écologie : {valeurEcologie}</p>
-                    <p>Valeur Économie : {valeurEconomie}</p>
-                    <p>Valeur Sociale : {valeurSocial}</p>
+                    <div className="flex justify-between space-x-3 p-5">
+                        <p className="rounded-md p-2 w-fit bg-green-200 text-green-700">Valeur Écologie : {valeurEcologie}</p>
+                        <p className="rounded-md p-2 w-fit bg-yellow-200 text-yellow-700">Valeur Économie : {valeurEconomie}</p>
+                        <p className="rounded-md p-2 w-fit bg-blue-200 text-blue-700">Valeur Sociale : {valeurSocial}</p>
+                    </div>
                     <h3>{textDeFin}</h3>
                 </div>
-                <Link to={"/"}>Terminer</Link>
+                <Link to={"/"} className="rounded-md p-2 w-fit bg-slate-200 text-black ml-[85%]">Terminer</Link>
             </Card>
         </div>
     );
