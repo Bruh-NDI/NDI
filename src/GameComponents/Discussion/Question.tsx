daa5f43548baded38ed14e55d360373895ecb1cd
0CT0PUCE-LOG <soren.starck@etu.umontpellier.fr>
2023-12-08 01:14:36 +0100

    center textbox

--- src/GameComponents/Discussion/Question.tsx
+++ src/GameComponents/Discussion/Question.tsx
@@ -5,7 +5,7 @@ export function Question({question, showNext}:{question: QuestionProps, showNext
 () => void
 }): any {
     return (
-        <div id={question.id} onClick={showNext} className="w-[40%] bg-white h-full cursor-pointer">
+        <div id={question.id} onClick={showNext} className="w-[40%] bg-white h-full cursor-pointer rounded-se-2xl">
             <p className="z-50 font-bold text-black ">{question.nomPersonnage} : {question.text}</p>
         </div>
     )
