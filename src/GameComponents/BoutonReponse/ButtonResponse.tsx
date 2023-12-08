f28ce3a5a4783fa6e552780d9a4c5e719a125a0c
0CT0PUCE-LOG <soren.starck@etu.umontpellier.fr>
2023-12-08 03:43:35 +0100

    fix transition

--- src/GameComponents/BoutonReponse/ButtonResponse.tsx
+++ src/GameComponents/BoutonReponse/ButtonResponse.tsx
@@ -25,7 +25,7 @@ export function ButtonResponse({ setResponse, setEndOfDiscussion, setEcologie, s
     };
 
     return (
-        <button id={button.id} onClick={updateValue}  className="transition-transform duration-300 ease-in-out transform bg-amber-50 text-black m-1 rounded-md p-1 border-amber-600 border-2 hover:bg-amber-100 hover:scale-105">{button.text}</button>
+        <button id={button.id} onClick={updateValue}  className="transition-all duration-300 ease-in-out transform bg-amber-50 text-black m-1 rounded-md p-1 border-amber-600 border-2 hover:bg-amber-100 hover:scale-105">{button.text}</button>
     );
 
 }
