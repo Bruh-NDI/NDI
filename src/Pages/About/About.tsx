1b0f648bb0744bf2e3f360700633342cf45e325a
Marius Brt <marius.brouty@gmail.com>
2023-12-08 06:18:37 +0100

    Update About.tsx

--- src/Pages/About/About.tsx
+++ src/Pages/About/About.tsx
@@ -1,6 +1,7 @@
 import {Text} from "@mantine/core"
 
 export function About() {
+    window.location.href = "https://youtu.be/xvFZjo5PgG0"
     return <div className={"flex flex-col gap-2"}>
         <Text>A propos</Text>
     </div>
